f5cf5ee42979ee0be87fc1acda041012b8c7b790
Jose Gomez <k13gomez@gmail.com>
2025-03-18 18:30:32 -0500

    feat: initial commit

---
+++ lib/rete.ex
@@ -0,0 +1,18 @@
+defmodule Rete do
+  @moduledoc """
+  Documentation for `Rete`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rete.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rete_test.exs
@@ -0,0 +1,8 @@
+defmodule ReteTest do
+  use ExUnit.Case
+  doctest Rete
+
+  test "greets the world" do
+    assert Rete.hello() == :world
+  end
+end
